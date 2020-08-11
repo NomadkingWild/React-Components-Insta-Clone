d0fb7cf85ecda7c9ee89661b443f798b63a2b3d7
NomadkingWild <67304919+NomadkingWild@users.noreply.github.com>
2020-08-11 17:05:03 -0400

    got my thumbnailsand posts to show need comments.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, {useState} from 'react';
+import Posts from './components/Posts/Posts'
+import SearchBar from './components/SearchBar/SearchBar'
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import dummyData from './dummy-data';
 
 const App = () => {
+  const[post, setPost]= useState(dummyData)
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const[searchbox, setSearchBox]= useState('')
 
   const likePost = postId => {
+
+    setPost(
+      post.map(po =>{
+        if(po.id === postId){
+          return{...po, likes: (po.likes+1)}
+        }
+        return po
+      })
+    )
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
-
+  
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
 
@@ -31,8 +45,11 @@ const App = () => {
 
   return (
     <div className='App'>
+    {/* Check the implementation of each component, to see what props they require, if any! */}<SearchBar />
+            <Posts  posts={post} likePost={likePost} />
       {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+
+      
     </div>
   );
 };
